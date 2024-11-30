1ffef694070d36b1eb86107a233df0ca86408cbe
Kuba Klin <poczta@kubaklin.pl>
2024-11-30 13:42:35 +0100

    feat: check draw function

---
+++ src/components/checkDraw.js
@@ -0,0 +1,6 @@
+export function checkDraw(playerXClass, playerOClass, cells) {
+  const boardCells = Array.from(cells);
+  return boardCells.every(function(cell) {
+    return cell.classList.contains(playerXClass) ||cell.classList.contains(playerOClass);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
 import './style.css';
-import { startGame } from "./components/startGame";
-import { checkWin } from "./components/checkWin";
+import { startGame } from './components/startGame';
+import { checkWin } from './components/checkWin';
+import { resetGame } from './components/resetGame';
+import { checkDraw } from './components/checkDraw';
 
 const firstPlayerNameInput = document.querySelector('.first-player-input');
 const secondPlayerNameInput = document.querySelector('.second-player-input');
 
-const playerO = 'circle';
-const playerX = 'cross';
-
-const firstPlayer = firstPlayerNameInput?.value;
-const secondPlayer = firstPlayerNameInput?.value;
-
 const startGameButton = document.querySelector('.start-game-button');
+const startWrapper = document.querySelector('.start-wrapper');
+const currentTurnWrapper = document.querySelector('.current-turn');
+
+const playerOClass = 'circle';
+const playerXClass = 'cross';
 
 const board = document.querySelector('.board');
 const cells = document.querySelectorAll('.cell');
+const bottomControls = document.querySelector('.winning-message-wrapper');
 const winningMessage = document.querySelector('.winning-message');
 const restartButton = document.querySelector('.restart-button');
 let isSecondPlayerTurn = false;
 
 startGameButton?.addEventListener('click', function() {
-  startGame(board, firstPlayerNameInput, secondPlayerNameInput);
+  startGame(board, firstPlayerNameInput, secondPlayerNameInput, currentTurnWrapper, startWrapper);
 })
 
 cells.forEach(function(cell) {
   cell.addEventListener('click', function() {
-    let currentTurn = isSecondPlayerTurn ? playerO : playerX;
+    const firstPlayerName = firstPlayerNameInput?.value;
+    const secondPlayerName = secondPlayerNameInput?.value;
+    let currentTurn = isSecondPlayerTurn ? playerXClass : playerOClass;
+    let currentPlayer = isSecondPlayerTurn ? firstPlayerName : secondPlayerName;
     cell.classList.add(currentTurn);
-    isSecondPlayerTurn = !isSecondPlayerTurn;
     if (checkWin(currentTurn, cells)) {
-      console.log(currentTurn + 'wins!!');
+      bottomControls?.classList.add('visible');
+      winningMessage.innerText = (`${isSecondPlayerTurn ? secondPlayerName : firstPlayerName} wins!`);
+    } if (checkDraw(playerXClass, playerOClass, cells)) {
+      bottomControls?.classList.add('visible');
+      winningMessage.innerText = ('It`s a draw!');
+    } else {
+      currentTurnWrapper.innerText = ('Current turn: ' + currentPlayer);
+      isSecondPlayerTurn = !isSecondPlayerTurn;
     }
   })
+})
+
+restartButton.addEventListener('click', function() {
+  cells.forEach(function(cell) {
+    cell.classList.remove('cross', 'circle');
+  })
+  resetGame(board, bottomControls, firstPlayerNameInput, secondPlayerNameInput, currentTurnWrapper, startWrapper);
 })
